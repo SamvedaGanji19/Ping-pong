35cc2c48576869e94f074b4f07a2e1bd0debff0b
SamvedaGanji19 <78354707+SamvedaGanji19@users.noreply.github.com>
2022-02-18 17:40:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas=createCanvas(380,380);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+    
+}
+
+function draw(){
+
+}
